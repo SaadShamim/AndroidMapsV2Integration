362d405e453acc6ad50e8b96b9e923f25449e8f0
Saad Shamim <saadshamim@gmail.com>
2013-01-21 06:03:50 -0800

    initial commit

---
+++ MapsProject/src/com/example/mapsproject/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mapsproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+public class MainActivity extends Activity {
+	
+	  private GoogleMap map;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+		        .getMap();
+	}
+
+
+
+}
